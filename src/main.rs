b564f80442e8f44ffeeb8e46e27a7d89b981f0f7
partoftheworlD <partoftheworld@vivaldi.net>
2024-04-23 23:48:01 +0300

    using a template macro, instead of rewriting code when a change is needed

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,23 @@ use windows::Win32::UI::Input::KeyboardAndMouse::{
     keybd_event, GetAsyncKeyState, KEYBD_EVENT_FLAGS, KEYEVENTF_KEYUP, VK_CAPITAL, VK_SPACE,
 };
 
+macro_rules! SpamKey {
+    ($activator: ident,$key: ident,$timeout: ident) => {
+        const KEYEVENTF_KEYDOWN: KEYBD_EVENT_FLAGS = KEYBD_EVENT_FLAGS(0);
+        let button = u8::try_from($key.0).unwrap();
+        if (GetAsyncKeyState($activator.0.into())) != 0 {
+            keybd_event(button, 0, KEYEVENTF_KEYDOWN, 0);
+            thread::sleep($timeout);
+            keybd_event(button, 0, KEYEVENTF_KEYUP, 0);
+        }
+    };
+}
+
 fn main() {
-    const KEYEVENTF_KEYDOWN: KEYBD_EVENT_FLAGS = KEYBD_EVENT_FLAGS(0);
     const TIMEOUT: Duration = Duration::from_millis(5);
-    let button = u8::try_from(VK_SPACE.0).unwrap();
     loop {
-        // VK_CAPITAL - CAPSLOCK
         unsafe {
-            if (GetAsyncKeyState(VK_CAPITAL.0.into())) != 0 {
-                keybd_event(button, 0, KEYEVENTF_KEYDOWN, 0);
-                thread::sleep(TIMEOUT);
-                keybd_event(button, 0, KEYEVENTF_KEYUP, 0);
-            }
+            SpamKey!(VK_CAPITAL, VK_SPACE, TIMEOUT);
         }
         thread::sleep(TIMEOUT);
     }
