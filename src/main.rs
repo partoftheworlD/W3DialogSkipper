3a12e2f759ba95e9a16c861ef610ee4fe6db9807
partoftheworlD <partoftheworld@vivaldi.net>
2024-04-24 01:13:15 +0300

    small optimization

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use windows::Win32::UI::Input::KeyboardAndMouse::{
 
 macro_rules! SpamKey {
     ($activator: ident,$key: ident,$timeout: ident) => {
-        const KEYEVENTF_KEYDOWN: KEYBD_EVENT_FLAGS = KEYBD_EVENT_FLAGS(0);
         let button = u8::try_from($key.0).unwrap();
         if (GetAsyncKeyState($activator.0.into())) != 0 {
             keybd_event(button, 0, KEYEVENTF_KEYDOWN, 0);
@@ -17,6 +16,7 @@ macro_rules! SpamKey {
 }
 
 fn main() {
+    const KEYEVENTF_KEYDOWN: KEYBD_EVENT_FLAGS = KEYBD_EVENT_FLAGS(0);
     const TIMEOUT: Duration = Duration::from_millis(5);
     loop {
         unsafe {
