035c838c8e962e3539adc5b8f2e6d474ecd9afc5
partoftheworlD <partoftheworld@vivaldi.net>
2024-05-11 12:47:45 +0300

    wrapping unsafe block

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,12 @@ use windows::Win32::UI::Input::KeyboardAndMouse::{
 macro_rules! SpamKey {
     ($activator: ident,$key: ident,$timeout: ident) => {
         let button = u8::try_from($key.0).unwrap();
-        if (GetAsyncKeyState($activator.0.into())) != 0 {
-            keybd_event(button, 0, KEYEVENTF_KEYDOWN, 0);
-            thread::sleep($timeout);
-            keybd_event(button, 0, KEYEVENTF_KEYUP, 0);
+        unsafe {
+            if (GetAsyncKeyState($activator.0.into())) != 0 {
+                keybd_event(button, 0, KEYEVENTF_KEYDOWN, 0);
+                thread::sleep($timeout);
+                keybd_event(button, 0, KEYEVENTF_KEYUP, 0);
+            }
         }
     };
 }
@@ -19,9 +21,7 @@ fn main() {
     const KEYEVENTF_KEYDOWN: KEYBD_EVENT_FLAGS = KEYBD_EVENT_FLAGS(0);
     const TIMEOUT: Duration = Duration::from_millis(5);
     loop {
-        unsafe {
-            SpamKey!(VK_CAPITAL, VK_SPACE, TIMEOUT);
-        }
+        SpamKey!(VK_CAPITAL, VK_SPACE, TIMEOUT);
         thread::sleep(TIMEOUT);
     }
 }
