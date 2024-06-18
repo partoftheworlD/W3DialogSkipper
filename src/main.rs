6c0c1aa0f0b951b1117b9efa95ccfef839322fef
partoftheworlD <partoftheworld@vivaldi.net>
2024-06-19 00:07:07 +0300

    test

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,24 @@
 use std::{thread, time::Duration};
 
 use windows::Win32::UI::Input::KeyboardAndMouse::{
-    keybd_event, GetAsyncKeyState, KEYBD_EVENT_FLAGS, KEYEVENTF_KEYUP, VK_CAPITAL, VK_SPACE,
+    keybd_event, GetAsyncKeyState, KEYBD_EVENT_FLAGS, KEYEVENTF_KEYUP, VIRTUAL_KEY, VK_CAPITAL, VK_SPACE
 };
 
-macro_rules! SpamKey {
-    ($activator: ident,$key: ident,$timeout: ident) => {
-        let button = u8::try_from($key.0).unwrap();
-        unsafe {
-            if (GetAsyncKeyState($activator.0.into())) != 0 {
-                keybd_event(button, 0, KEYEVENTF_KEYDOWN, 0);
-                thread::sleep($timeout);
-                keybd_event(button, 0, KEYEVENTF_KEYUP, 0);
+fn safe_spam(vkey: VIRTUAL_KEY, dur: Duration, button: VIRTUAL_KEY) {
+    const KEYEVENTF_KEYDOWN: KEYBD_EVENT_FLAGS = KEYBD_EVENT_FLAGS(0);
+    unsafe {
+            if (GetAsyncKeyState(vkey.0.into())) != 0 {
+                println!("vkey={} button={}", vkey.0, button.0);
+                keybd_event(button.0.try_into().unwrap(), 0, KEYEVENTF_KEYDOWN, 0);
+                thread::sleep(dur);
+                keybd_event(button.0.try_into().unwrap(), 0, KEYEVENTF_KEYUP, 0);
             }
         }
-    };
 }
-
 fn main() {
-    const KEYEVENTF_KEYDOWN: KEYBD_EVENT_FLAGS = KEYBD_EVENT_FLAGS(0);
-    const TIMEOUT: Duration = Duration::from_millis(5);
+    const TIMEOUT: Duration = Duration::from_millis(100);
     loop {
-        SpamKey!(VK_CAPITAL, VK_SPACE, TIMEOUT);
+        safe_spam(VK_CAPITAL,TIMEOUT, VK_SPACE);
         thread::sleep(TIMEOUT);
-    }
+    }   
 }
